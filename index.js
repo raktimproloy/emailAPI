d4a495c8d71d1c1e1a232bde72984b260e08fc1f
raktimproloy <raktimproloy@gmail.com>
2023-07-03 05:10:18 +0100

    first push

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+const contactUs = require("./routerHandler/contactUsHandle.js")
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use("/contactus", contactUs)
+
+
+const errorHandler = (err, req, res, next) => {
+    if(res.headerSent){
+        return next(err)
+    }
+    res.status(500).json({error: err})
+}
+app.use(errorHandler)
+
+app.listen(3001, () => {
+    console.log(`Listening 3000`);
+})
---
+++ routerHandler/contactUsHandle.js
@@ -0,0 +1,70 @@
+const express = require("express")
+const router = express.Router()
+const nodemailer = require("nodemailer")
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'raktimproloy@gmail.com',
+      pass: 'ohyhbbihkqmslcfp'
+    }
+  });
+
+router.put("/send/:email", async(req, res) => {
+    try {
+        const gmailCheck = /([a-zA-Z0-9]+)([\.{1}])?([a-zA-Z0-9]+)\@gmail([\.])com/g
+        const peramsSlice = req.params.email.split("&")
+        if(peramsSlice.length === 2){
+            const peramsTo = peramsSlice[0].split(":")
+            const peramsSub = peramsSlice[1].split(":")
+            if(peramsTo[0] === "to" && peramsSub[0] === "sub"){
+                if(peramsTo[0] !== "" && peramsTo[1].match(gmailCheck)){
+                    const data = req.body
+                    const objectKey = Object.keys(data)
+                    let text = ""
+                    const subjectText =  peramsSub[1].replace("_", " ")
+            
+                    for (let index = 0; index < objectKey.length; index++) {
+                        text = text + `${objectKey[index]}: ${data[objectKey[index]]} \n`
+                    }
+            
+                    var mailOptions = {
+                        from: 'raktimproloy@gmail.com',
+                        to: peramsTo[1],
+                        subject: subjectText,
+                        text:  text
+                    };
+                    transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        res.status(500).json({
+                            error: "Server side error"
+                        })
+                    } else {
+                        res.status(200).json({
+                            message: "Email sent successful"
+                        })
+                    }
+                    });
+                }else{
+                    res.status(500).json({
+                        error: "Server side error"
+                    })
+                }
+            }else{
+                res.status(500).json({
+                    error: "Server side error"
+                })
+            }
+        }else{
+            res.status(500).json({
+                error: "Server side error"
+            })
+        }
+    } catch (error) {
+        res.status(404).json({
+            error: "Server side error"
+        })
+    }
+})
+
+module.exports = router
